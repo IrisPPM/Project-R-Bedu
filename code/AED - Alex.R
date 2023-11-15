06d216604b44aec70854cf9519afa5b13c75b0d4
unpesodecambio <76847763+unpesodecambio@users.noreply.github.com>
2023-11-14 18:03:47 -0600

    Update AED - Alex.R

--- code/AED - Alex.R
+++ code/AED - Alex.R
@@ -1,4 +1,37 @@
 ## 14-NOV
 ##AED - ALEJANDRO FLORES C
-alex = 34
-alex
+
+
+#cargar paquetes
+if (!require("tidyverse")) install.packages("tidyverse")
+library(tidyverse)
+
+#cargar dataset
+smokers <- read.csv("data/Smokers.csv")
+smokers
+view(smokers)
+
+
+#exploración inicial
+head(smokers)
+summary(smokers)
+str(smokers)
+dim(smokers)
+
+#datos faltantes
+colSums(is.na(smokers))
+#NO HAY DATOS FALTANTES
+
+
+#gráficas
+hist(smokers$age)
+
+plot(smokers$age, smokers$Cholesterol)
+
+boxplot(smokers$age)
+
+barplot(table(smokers$Cholesterol))
+
+heatmap(cor(smokers))
+
+median(age)
