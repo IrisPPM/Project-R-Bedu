f500a9fa4affd94118d8b4cb6661c600e711e1e5
unpesodecambio <76847763+unpesodecambio@users.noreply.github.com>
2023-11-14 18:04:33 -0600

    Update AED - Alex.R

--- code/AED - Alex.R
+++ code/AED - Alex.R
@@ -4,7 +4,10 @@
 
 #cargar paquetes
 if (!require("tidyverse")) install.packages("tidyverse")
+install.packages("corrplot")
 library(tidyverse)
+library(readr)
+library(corrplot)
 
 #cargar dataset
 smokers <- read.csv("data/Smokers.csv")
@@ -18,10 +21,15 @@ summary(smokers)
 str(smokers)
 dim(smokers)
 
+nrow(smokers)
+ncol(smokers)
+
 #datos faltantes
 colSums(is.na(smokers))
 #NO HAY DATOS FALTANTES
 
+head(smokers)
+tail(smokers)
 
 #gráficas
 hist(smokers$age)
@@ -34,4 +42,12 @@ barplot(table(smokers$Cholesterol))
 
 heatmap(cor(smokers))
 
-median(age)
+
+"""
+correlación especial
+"""
+correlation_matrix <- cor(smokers[, -1])
+correlation_matrix
+par(mar = c(1, 1, 1, 1))
+corrplot(correlation_matrix, method = "color", col = colorRampPalette(c("blue", "white", "red"))(100),
+         tl.col = "black", tl.cex = 0.7)
